b1f142633f0c20e9294c5a3f34d29978db0fe02b
biswass101 <biswassnaeemcse@gmail.com>
2025-02-18 12:29:43 +0600

    added pipe method

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.listen(7000, '127.0.0.1', () => {
     console.log("http://localhost:"+'7000')
 })
 
+//Solution1:
 //not applicable for large file //app might crash
 // server.on('request', (req, res) => {
 //     fs.readFile('./Files/20M.txt', (err, data) => {
@@ -21,18 +22,39 @@ server.listen(7000, '127.0.0.1', () => {
 //     })
 // })
 
-//Solutin: Using readable and writable stream-->
-server.on('request', (req, res) => {
-    let rs = fs.createReadStream('./Files/2M.txt')
+//Solutin2: Using readable and writable stream-->
+//(cons: speed mismatch of readable and writable stream called backpressure)
+// server.on('request', (req, res) => {
+//     let rs = fs.createReadStream('./Files/20M.txt')
 
-    rs.on('data', (chunk) => {
-        res.write(chunk)
-        res.end()
-    })
+//     rs.on('data', (chunk) => {
+//         res.write(chunk)
+        
+//     })
+
+//     rs.on('end', () => {
+//         res.end()
+//     })
+
+//     //err handling
+//     rs.on('error', (err) =>{
+//         res.end(err.message)
+//     })
+// })
+
+//Solution3: Using Pipe
+server.on('request', (req, res) => {
+    let rs = fs.createReadStream('./Files/20M.txt')
+
+    //here speed of the data passing and recieveing is controlled
+    rs.pipe(res) 
 
     //err handling
     rs.on('error', (err) =>{
         res.end(err.message)
     })
 })
+
+
+
 
